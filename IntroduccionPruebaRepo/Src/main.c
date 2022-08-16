eb5499a79c25c77a4a83119d81a67e9bf782177b
felipegc00 <jfguerreroca@unal.edu.co>
2022-08-16 09:59:31 -0500

    Update main.c

--- IntroduccionPruebaRepo/Src/main.c
+++ IntroduccionPruebaRepo/Src/main.c
@@ -19,12 +19,11 @@
 
 #include <stdint.h>
 
-#if !defined(__SOFT_FP__) && defined(__ARM_FP)
-  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
-#endif
+#define NUMERO_DE_VUELTAS 25
+
 
 int main(void)
 {
     /* Loop forever */
-	for(;;);
+
 }
