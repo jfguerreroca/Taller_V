a8988911c02085f91d1b9f601a1c94ed26ebb8e5
felipegc00 <jfguerreroca@unal.edu.co>
2022-09-01 10:43:24 -0500

    BasicConfig
    Configuracion basica para proyectos realizada en clase

---
+++ BasicConfig/Src/main.c
@@ -0,0 +1,25 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : jfguerreroca
+ * @brief          : Configuración básica de un proyecto
+ ******************************************************************************
+ *
+ ******************************************************************************
+ */
+
+#include <stdint.h>
+
+/**
+ * Función principal del programa
+ * Esta funcion es el corazón del programa
+ */
+int main(void)
+{
+    /* Loop forever */
+	while(1){
+
+	}
+
+	return 0;
+}
