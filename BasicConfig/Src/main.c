ff66ce8cc486d3b3fb785618419c3d334f864baa
felipegc00 <jfguerreroca@unal.edu.co>
2022-09-01 11:59:22 -0500

    Ejemplo de operadores y punteros

--- BasicConfig/Src/main.c
+++ BasicConfig/Src/main.c
@@ -13,9 +13,19 @@
 /**
  * Función principal del programa
  * Esta función es el corazón del programa
- */
+ * */
+
+uint8_t dummy = 0;
+uint8_t *ptrEjemplo = 0;
+uint8_t valorPuntero = 0;
+
 int main(void)
 {
+
+	dummy = 30;
+	ptrEjemplo = &dummy
+	*ptrEjemplo += 45;
+	valorPuntero = (int)ptrEjemplo;
     /* Loop forever */
 	while(1){
 
