f13620fcead33df24167128feb043e4ff62a2e72
felipegc00 <jfguerreroca@unal.edu.co>
2022-09-02 23:01:59 -0500

    Tarea 2 Actualizacion
    Punto 1 Ready

--- SolucionTarea2/Src/main.c
+++ SolucionTarea2/Src/main.c
@@ -15,6 +15,7 @@
 uint8_t estadoPin1 = 0;
 uint8_t estadoPin2 = 0;
 
+
 int main(void)
 {
 
@@ -24,26 +25,26 @@ int main(void)
 
 	// Deseamos trabajar con el puerto GPIOC
 	handlerUserPin1.pGPIOx = GPIOC;
-	handlerUserPin1.GPIO_PinConfig.GPIO_PinNumber			= PIN_9;
+	handlerUserPin1.GPIO_PinConfig.GPIO_PinNumber			= PIN_6;
 	handlerUserPin1.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
 	handlerUserPin1.GPIO_PinConfig.GPIO_PinOPType			= GPIO_OTYPE_PUSHPULL;
-	handlerUserPin1.GPIO_PinConfig.GPIO_PinPuPdControl		= GPIO_PUPDR_PULLUP;
+	handlerUserPin1.GPIO_PinConfig.GPIO_PinPuPdControl		= GPIO_PUPDR_NOTHING;
 	handlerUserPin1.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_MEDIUM;
 	handlerUserPin1.GPIO_PinConfig.GPIO_PinAltFunMode		= AF0;						//Ninguna funcion
 
 	handlerUserPin2.pGPIOx = GPIOC;
-	handlerUserPin2.GPIO_PinConfig.GPIO_PinNumber			= PIN_10;
+	handlerUserPin2.GPIO_PinConfig.GPIO_PinNumber			= PIN_13;
 	handlerUserPin2.GPIO_PinConfig.GPIO_PinMode				= GPIO_MODE_OUT;
 	handlerUserPin2.GPIO_PinConfig.GPIO_PinOPType			= GPIO_OTYPE_PUSHPULL;
 	handlerUserPin2.GPIO_PinConfig.GPIO_PinPuPdControl		= GPIO_PUPDR_NOTHING;
 	handlerUserPin2.GPIO_PinConfig.GPIO_PinSpeed			= GPIO_OSPEED_MEDIUM;
 	handlerUserPin2.GPIO_PinConfig.GPIO_PinAltFunMode		= AF0;						//Ninguna funcion
 
-	// Cargamos al configuracion del PIN_9 y PIN_10 especifico
+	// Cargamos al configuracion del PIN_6 y PIN_13 especifico
 	GPIO_Config(&handlerUserPin1);
 	GPIO_Config(&handlerUserPin2);
 
-	// Hacemos que el PIN_9 quede encendido
+	// Hacemos que el PIN_6 quede encendido
 	GPIO_WritePin(&handlerUserPin1, SET);
 
 	// Leemos el estado de varios pines para comprobar la funcion ReadPin
