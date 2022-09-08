a4814f013dfd5d0df97b60e1444ff2829fc897fd
felipegc00 <jfguerreroca@unal.edu.co>
2022-09-08 11:54:59 -0500

    Clase de Timers

--- BasicConfig/Src/main.c
+++ BasicConfig/Src/main.c
@@ -9,23 +9,36 @@
  */
 
 #include <stdint.h>
+#include <stdbool.h>
+#include "BasicTimer.h"
+#include "GPIOxDriver.h"
 
 /**
  * Función principal del programa
  * Esta función es el corazón del programa
  * */
 
-uint8_t dummy = 0;
-uint8_t *ptrEjemplo = 0;
-uint8_t valorPuntero = 0;
+BasicTimer_Handler_t 	handlerTimer2 		= {0};
+GPIO_Handler_t 			handlerBlinkyLed 	= {0};
 
 int main(void)
 {
 
-	dummy = 30;
-	ptrEjemplo = &dummy
-	*ptrEjemplo += 45;
-	valorPuntero = (int)ptrEjemplo;
+	handlerBlinkyLed.pGPIOx = GPIOA;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinNumber = PIN_5;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OTYPE_PUSHPULL;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_OSPEED_FAST;
+	handlerBlinkyLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPDR_NOTHING;
+	GPIO_Config(&handlerBlinkyLed);
+	GPIO_WritePin(&handlerBlinkyLed, SET);
+
+	handlerTimer2.ptrTIMx = TIM2;
+	handlerTimer2.TIMx_Config.TIMx_mode = BTIMER_MODE_UP;
+	handlerTimer2.TIMx_Config.TIMx_speed = BTIMER_SPEED_100us;
+	handlerTimer2.TIMx_Config.TIMx_period = 2500;
+	handlerTimer2.TIMx_Config.TIMx_interruptEnable = 1;
+
     /* Loop forever */
 	while(1){
 
